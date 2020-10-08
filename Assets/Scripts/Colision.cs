98c8aaacd95c168ce9566223cdd09ff6c5947ba4
JuoK <rnjswndh@gmail.com>
2020-10-08 10:13:36 +0900

    업로드 연습
    유니티 프로젝트 업로드 연습

---
+++ Assets/Scripts/Colision.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Colision : MonoBehaviour
+{
+    //trigger
+    /*
+     //가로막히는 것들 (화장실 들어가는데 불켜지는 현상)
+     물리연산은 없음 / 충돌검사는 함 
+
+     충돌 된 순간 호출
+     충돌 지속시 호출
+     충돌 끝날시
+
+     Enter / Stay / Exit 
+    */
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    //Collider 충돌체 컴포넌트
+    //    //충돌체 둘중 하느는 regitbody가 있어야 함;
+    //    string str = "Trigger Enter : " + other.name;
+    //    print(str);
+    //}
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    string str = "Trigger Stay : " + other.name;
+    //    print(str);
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    string str = "Trigger Exit : " + other.name;
+    //    print(str);
+    //}
+
+
+
+    /*
+     Collision
+     가로막혀 있으면 못 지나감;
+     */
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        //Collision 충돌 된 정보를 가지고 있음;
+        //Collider 최상위
+
+        string str = "Collision Enter :" + collision.gameObject.name;
+        print(str);    
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        string str = "Collision Stay :" + collision.gameObject.name;
+        print(str);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        string str = "Collision Exit :" + collision.gameObject.name;
+        print(str);
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Move: MonoBehaviour 
+{
+    private void Update()
+    {
+        Vector3 position = this.transform.position;
+
+        
+        //keycode는 enum으로 만듬;
+        //GetKey는 프레스
+        //왼손좌표계 z 가 증가할 경우 안으로 들어감;
+
+        //반환받는 거 ㅅ까지는 되는데 this.transform.position을 반환할 수 없기 때문에 position을 만들어서 받음;
+        //ctrl + shift + f 카메라 위치 이동;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            position.z += 10 * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            position.z -= 10 * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            position.x -= 10 * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            position.x += 10 * Time.deltaTime;
+        }
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    position.x += 10 * Time.deltaTime;
+        //}
+
+        this.transform.position = position;
+    }
+}
