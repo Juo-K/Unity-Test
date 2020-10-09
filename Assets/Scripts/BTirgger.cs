77d9a4cb0e2430895369411e059e42f509cebfb1
JuoK <rnjswndh@gmail.com>
2020-10-09 21:39:41 +0900

    충돌
    201009 충돌실험

---
+++ Assets/Scripts/BTirgger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class BTirgger : MonoBehaviour
+{
+
+    private Vector3 _position;
+    private Vector3 _CPositon;
+    private Vector3 _Direction;
+
+    private Rigidbody _rigidbody = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Cube")
+        {
+            
+
+
+            //_Direction = other.transform.position - _position;
+            //_CPositon = other.transform.position;
+
+            //_rigidbody = other.GetComponent<Rigidbody>();
+            //_rigidbody.useGravity = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision cother)
+    {
+        if (cother.collider.name == "Cube")
+        _Direction = cother.transform.position - _position;
+        _CPositon = cother.transform.position;
+
+
+        _CPositon += _Direction * 2;
+        //_rigidbody = cother.rigidbody.GetComponent<Rigidbody>();
+        //_rigidbody.useGravity = false;
+    }
+
+    private void Start()
+    {
+        _position = this.transform.position;
+
+    }
+
+   
+
+   
+}
