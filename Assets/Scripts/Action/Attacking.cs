9ed111bcfab61fdad669e961df84886fac5d8db0
JuoK <rnjswndh@gmail.com>
2020-10-16 21:42:25 +0900

    201016
    적군 캐릭 생성

---
+++ Assets/Scripts/Action/Attacking.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacking : MonoBehaviour, IAction
+{
+    private ActionManager _actionManager = null;
+    private Animator _animator = null;
+    private Vector3 _mPos;
+
+
+    public void Beging(object initValue)
+    {
+        Damage target = initValue as Damage; // Damage로 변할 수 있으면 반환 아니면 null
+        Debug.Assert(target != null, "입력 가능 자료형  : Damage");
+
+        _actionManager.StartAction(this); // 스크립트 자체를 넘김;
+
+    }
+
+    public void End()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void Awake()
+    {
+        _actionManager = this.GetComponent<ActionManager>();
+        _animator = this.GetComponent<Animator>();
+    }
+   
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(1))
+        //{
+
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//화면좌표계 상의 좌표를 반직선으로 바꿈; 바꾸고 싶은 값을 안에 집어넣음;
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(ray, out hit, 500))//물체의 좌표까지 무조건감;
+        //    {
+        //        _mPos = hit.point;
+
+        //    }
+        //    if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Racer_Pench")  == false)
+        //    {
+        //        _animator.SetTrigger("Attack");
+        //        this.transform.LookAt(_mPos);
+        //    }
+
+        //}
+
+    }
+
+    private void OnAttack()
+    {
+        Debug.Log("Attack!");
+    }
+
+    private void OnAttackEnd()
+    {
+        _animator.ResetTrigger("Attack");
+    }
+
+  
+}
---
+++ Assets/Scripts/Action/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //객체 스크립트를 가지고 있을 거입니다.
+
+    private Moving _moving = null;
+
+    private void Awake()
+    {
+        _moving = this.GetComponent<Moving>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       // if()
+        if (Moving() == true)  return;
+    }
+
+    private bool Moving()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(GetMouseRay(), out hit, 500))
+            {
+                _moving.Beging(hit.point);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Ray GetMouseRay()
+    {
+       return Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
+}
